1bca65e3df63007724f57a0b17a1ec444fe4b506
zacker <dev.zacker@gmail.com>
2024-11-03 20:47:46 +0900

    feat: publicな環境変数をPrefixをつけないで登録する

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ import {ConfigContext} from "@expo/config"
 export default ({config}: ConfigContext) => {
     config.extra = {
         ...config.extra,
+        APP_NAME: process.env.APP_NAME,
         API_KEY: process.env.API_KEY,
     }
     return config;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,9 @@ export default function HomeScreen() {
           API_KEY: {Constants.expoConfig?.extra?.API_KEY}
         </Text>
         <Text>
+          APP_NAME: {Constants.expoConfig?.extra?.APP_NAME}
+        </Text>
+        <Text>
           EXPO_PUBLIC_APP_VERSION: {process.env.EXPO_PUBLIC_APP_VERSION}
         </Text>
     </View>
